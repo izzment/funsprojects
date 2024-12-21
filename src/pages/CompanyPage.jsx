ee3c7416b2d6435a73049c5cdab8bdde66c9c6a7
hilmi2004 <149455231+hilmi2004@users.noreply.github.com>
2024-12-21 21:21:40 +0100

    Update CompanyPage.jsx

--- src/pages/CompanyPage.jsx
+++ src/pages/CompanyPage.jsx
@@ -1,7 +1,5 @@
-import { useState } from "react";
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import SecondarySidebar from "../components/common/SecondarySidebar";
-
 import EmployeeTable from "../components/Companies/EmployeeTable";
 import Department from "../components/Companies/Department";
 import Designation from "../components/Companies/Designation";
@@ -17,34 +15,18 @@ const companySidebar = [
 ];
 
 export const CompanyPage = () => {
-  // THIS ENSURES THAT COMPANIES IS THE DEFAULT ONE SELECTED
-  const [selectedMenu, setSelectedMenu] = useState("/companies");
-  const navigate = useNavigate();
-
-  const handleMenuClick = (href) => {
-    setSelectedMenu(href);
-    navigate(href);
-  };
-
   return (
     <div className="flex">
-      <SecondarySidebar
-        links={companySidebar}
-        selected={selectedMenu}
-        onMenuClick={handleMenuClick}
-      />
+      <SecondarySidebar name={companySidebar} />
       <div className="w-5/6">
         <Routes>
-          <Route path="/" element={<EmployeeTable />} />
-          <Route path="/department" element={<Department />} />
-          <Route path="/designation" element={<Designation />} />
-          <Route path="/exitdetails" element={<ExitDetails />} />
-          <Route path="/org-tree" element={<OrganizationTree />} />
-          {/* This will help the detailing */}
-          <Route path="*" element={<div>Page Not Found</div>} />
+          <Route path="/companies" element={<EmployeeTable />} />
+          <Route path="/companies/department" element={<Department />} />
+          <Route path="/companies/designation" element={<Designation />} />
+          <Route path="/companies/exitdetails" element={<ExitDetails />} />
+          <Route path="/companies/org-tree" element={<OrganizationTree />} />
         </Routes>
       </div>
     </div>
   );
 };
-//change
