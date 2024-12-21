854f92c7abf9684405b667cf71b099b5cfaf3c0a
hilmi2004 <149455231+hilmi2004@users.noreply.github.com>
2024-12-21 20:53:11 +0100

    Update SecondarySidebar.jsx

--- src/components/common/SecondarySidebar.jsx
+++ src/components/common/SecondarySidebar.jsx
@@ -1,28 +1,25 @@
-import {NavLink} from "react-router-dom";
-import {useState} from "react";
+import { NavLink } from "react-router-dom";
 
-const SecondarySidebar = ({name}) => {
+const SecondarySidebar = ({ name }) => {
+  return (
+    <div className="w-1/6 bg-dusky-purple ml-[-70px] flex flex-col gap-2 py-8 px-4">
+        {/* More accurate mapping{Active} */}
+      {name.map((item) => (
+        <NavLink
+          key={item.href}
+          to={item.href}
+          end 
+          className={({ isActive }) =>
+            isActive
+              ? "px-8 py-2 text-dusky-purple rounded-lg bg-native font-semibold"
+              : "px-8 py-2 font-thin hover:font-medium hover:bg-gray-200 rounded-lg"
+          }
+        >
+          {item.title}
+        </NavLink>
+      ))}
+    </div>
+  );
+};
 
-    const [selected, setSelected] = useState(false)
-    function isSelected(e) {
-        setSelected(true)
-    }
-    // comment comment
-    return (
-        <div className='w-1/6 bg-dusky-purple ml-[-70px] flex flex-col gap-2 py-8 px-4'>
-
-                {name.map((item) =>
-                        <NavLink to={item.href} className={({isActive}) =>
-                        isActive
-                        ? 'px-8 py-2 text-dusky-purple rounded-lg bg-native font-semibold'
-                        : 'px-8 py-2 font-thin hover:font-medium'
-                        }
-                        >
-                            {item.title}
-                </NavLink>
-                )}
-        </div>
-    )
-}
-
-export default SecondarySidebar
+export default SecondarySidebar;
